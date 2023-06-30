89e6a59a3751bb274d36a15ed9cc726a9fa3f2d8
bitt6 <103851865+bitt6@users.noreply.github.com>
2023-06-30 22:25:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'omegalinks.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'd43e60fbfa89898f317bbea832f57f97fc669e2a')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'shareus.io')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'BxAXtVJ8LlgEYZFcBltF6CLxRVJ3')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
