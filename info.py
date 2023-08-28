7ccb5952077c5e7e54201e214e853f0965e46931
FOZIA QURAISHI <103851865+bitt6@users.noreply.github.com>
2023-08-28 23:38:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -78,7 +78,7 @@ if SELF_DELETE == "True":
     SELF_DELETE = True
 
     # Download Tutorial Button #
-DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
+DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD📥"
 DOWNLOAD_TEXT_URL = "https://omegalinks.in/ref/GreyMatter6458"
 
    # Custom Caption Under Button #
